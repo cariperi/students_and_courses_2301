51b05ff9b4ad17adfd2235b94b083db22d9d6246
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-15 11:21:37 -0500

    test: add test for initialize method to Course class

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,27 @@ require './lib/student'
 require './lib/course'
 
 describe Course do
+  before(:each) do
+    @course = Course.new("Calculus", 2)
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})
+  end
 
+  describe '#initialize' do
+    it 'exists' do
+      expect(@course).to be_a Course
+    end
+
+    it 'has a name' do
+      expect(@course.name).to eq("Calculus")
+    end
+
+    it 'has a capacity' do
+      expect(@course.capacity).to eq(2)
+    end
+
+    it 'starts with no students by default' do
+      expect(@course.students).to eq([])
+    end
+  end
 end
