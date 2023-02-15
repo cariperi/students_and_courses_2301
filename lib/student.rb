c2e248c1f861e7193e48c7703dc70cbf4cbaf223
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-15 11:10:33 -0500

    feat: add initialize method to Student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,1 +1,11 @@
+class Student
+  attr_reader :name,
+              :age,
+              :scores
 
+  def initialize(student_details)
+    @name = student_details[:name]
+    @age = student_details[:age]
+    @scores = []
+  end
+end
