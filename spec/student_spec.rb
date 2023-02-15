d7221c2a33cf49ab30c6e63fbefe574c0985b2d8
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-15 11:12:52 -0500

    test: add test for log_score method to Student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -23,4 +23,15 @@ describe Student do
       expect(@student.scores).to eq([])
     end
   end
+
+  describe '#log_score' do
+    it 'can keep track of student scores' do
+      expect(@student.scores).to eq([])
+
+      @student.log_score(89)
+      @student.log_score(78)
+
+      expect(@student.scores).to eq([89, 78])
+    end
+  end
 end
